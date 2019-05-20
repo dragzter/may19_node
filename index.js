1bc98a295cc9489bc47780009723c9f7ee09aa89
vm <vlad@q.local>
2019-05-20 00:52:59 -0400

    debug

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ app.use(express.static('public'))
 
 var router = express.Router();
 
-router.get('/api/test', function(req, res) {
+router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
 app.use('/api', router);
+
 app.listen(3000, () => console.log('Server running on port 3000'))
 
