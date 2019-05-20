5ee48381a8fbaae0bb686a0105dac0f926190872
vm <vlad@q.local>
2019-05-20 00:49:57 -0400

    mongoose

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ const app = express()
 
 app.use(express.static('public'))
 
+var router = express.Router();
+
+router.get('/api/test', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+app.use('/api', router);
 app.listen(3000, () => console.log('Server running on port 3000'))
+
