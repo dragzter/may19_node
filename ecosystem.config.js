30794205077bf88c72609321b2248be995ea5b17
vm <vlad@q.local>
2019-05-19 20:07:54 -0400

    setup pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+      name: 'tutorial-2',
+      script: './index.js'
+    }],
+    deploy: {
+      production: {
+        user: 'ubuntu',
+        host: 'ec2-35-153-199-70.compute-1.amazonaws.com',
+        key: '~/.ssh/may_19_2019.pem',
+        ref: 'origin/master',
+        repo: 'git@github.com:dragzter/may19_node.git',
+        path: '/home/ubuntu/test',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
