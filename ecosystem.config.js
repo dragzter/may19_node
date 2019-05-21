fd40393cff8d7378801c1099b2abc5465dc277ac
vm <vlad@q.local>
2019-05-20 21:36:38 -0400

    ecosystem update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     deploy: {
       production: {
         user: 'ubuntu',
-        host: 'ec2-35-153-199-70.compute-1.amazonaws.com',
+        host: 'ec2-54-80-177-249.compute-1.amazonaws.com',
         key: '~/.ssh/may_19_2019.pem',
         ref: 'origin/master',
         repo: 'git@github.com:dragzter/may19_node.git',
