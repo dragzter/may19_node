1beca1f8c71b853f7c1b25d414a3012d1d052f2d
vm <vlad@q.local>
2019-05-20 22:45:25 -0400

    error handling

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ const app = express()
 
 // Routes
 let personRoute = require('./server/routes/person')
+let path = require('path')
+
+// Middleware chain
+app.use((req, res, next) => {
+    console.log(`${new Date().toString()} => ${req.originalUrl}`)
+
+    next()
+})
 
 app.use(personRoute)
-
-// Serve the static content
 app.use(express.static('public'))
 
+// 404 - Not Found
+app.use((req, res, next) => {
+    res.status(404).send('Not Found - 404')
+})
+
+// Handler for error 500
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+
+    res.sendFile(path.join(__dirname, 'public/500.html'))
+})
 
 // Server port config
 const PORT = process.env.PORT || 3000
---
+++ server/models/customer.model.js
@@ -0,0 +1,6 @@
+let mongoose = require('mongoose')
+
+const server = ''
+const database = ''
+const user = ''
+const password = ''
